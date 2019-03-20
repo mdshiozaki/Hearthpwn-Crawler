0bd4101c3f4ec05c81b974da3274aef3bf51c377
mdshiozaki <michaels710@live.com>
2019-03-19 23:53:18 -0400

    Added Automatic Crawling
    Spider now crawls a search page and executes scrape on each link, then
    goes to the next page and repeats
    
    note: database statements currently commented out

--- hearthpwn/spiders/hearthpwn_spider.py
+++ hearthpwn/spiders/hearthpwn_spider.py
@@ -8,18 +8,18 @@ class HearthPwnSpider(scrapy.Spider):
 	name = "hearthpwn"
 	conn = None
 
-	start_urls = ['https://www.hearthpwn.com/decks/1238737-legend-control-warlock',
-			'https://www.hearthpwn.com/decks/1240710-control-otk-mage-poor-players-like-me', 
-			'https://www.hearthpwn.com/decks/1238561-pogo-rogue',
+	start_urls = [#start page with the filters for recent patch and standard applied
+			'https://www.hearthpwn.com/decks?filter-build=47&filter-show-standard=1&filter-deck-tag=5', 
 		]
 
 	def connectToDatabase(self):
 		try: 
+			# database connection established
 			self.conn = mysql.connector.connect(host='localhost', 
 				database='test_hsdecks',
 				user='root',
 				password='Accessdenied1',
-				auth_plugin='mysql_native_password')
+				auth_plugin='mysql_native_password') 
 
 			if self.conn.is_connected():
 				print('Connected to test_hsdecks')
@@ -29,12 +29,23 @@ class HearthPwnSpider(scrapy.Spider):
 
 
 	def parse(self, response):
+		page_links = response.css('span.tip a::attr(href)').getall() # gets the 25 links in the table on the search page
+
+		for link in page_links:
+			next_link = response.urljoin(link) #link is in the form /decks/737557-aggro-token-druid, urljoin links it to the main url
+			print(next_link)
+			yield scrapy.Request(next_link, callback=self.parse_deck) #calls parse_deck to scrape the data on the given link
+
+		next_page = response.urljoin(response.css('li.b-pagination-item-next a::attr(href)').get()) # searches for next button at bottom of page
+		yield scrapy.Request(next_page, callback=self.parse) #cycles back to the beginning to do this for page 2, 3,...
+
+
+	def parse_deck(self, response):
 		# if tavern brawl deck, don't add it
 		deckType = response.css('span.deck-type::text').get()
-		if deckType == 'Tavern Brawl':
-			return
 
 		self.connectToDatabase()
+
 		data = {}
 		data['deck'] = []
 		for deckcode in response.css('html'):
@@ -43,25 +54,27 @@ class HearthPwnSpider(scrapy.Spider):
 			fullDeck = deckstrings.parse_deckstring(code)
 			print(fullDeck)
 
-			query = "INSERT INTO test1(deckcode,url) " \
-			"VALUES(%s,%s)"
-			args = (code, link)
+			# query = "INSERT INTO test1(deckcode,url) " \
+			# "VALUES(%s,%s)"
+			# args = (code, link)
 
-			cursor = self.conn.cursor()
-			result = cursor.execute(query, args)
-			self.conn.commit()
+			# cursor = self.conn.cursor()
+			# result = cursor.execute(query, args)
+			# self.conn.commit()
+
+			# last_ID = "SELECT deck_id FROM test1 ORDER BY deck_id DESC LIMIT 1" # assumes there is at least 1 row in the table
 
 
 			#get last ID and add 1 to it
 			for i in range(len(fullDeck[0])):
-				card = fullDeck[0][i][0]
+				card = fullDeck[0][i][0] #cycles through the first array in the list, pulling the first item which is the card number
 				print(card)
 
-				card_query = "INSERT INTO test2(card_number) " \
-				"VALUES(%s)"
-				args = (card)
+				# card_query = "INSERT INTO test2(card_number) " \
+				# "VALUES(%s)"
+				# args = (card)
 
-				card_cursor = self.conn.cursor()
-				card_cursor.execute(card_query, (args, ))
-				self.conn.commit()
+				# card_cursor = self.conn.cursor()
+				# card_cursor.execute(card_query, (args, ))
+				# self.conn.commit()
 				
